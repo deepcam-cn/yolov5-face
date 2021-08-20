b8d9b0bbb33f5c0d96a2bec770470f5389d46f1c
Mr.Li <1055271769@qq.com>
2021-08-20 23:10:02 +0800

    add  support TensorRT

---
+++ torch2tensorrt/main.py
@@ -0,0 +1,100 @@
+import os
+import sys
+import cv2
+import copy
+import torch
+root_path=os.path.dirname(os.path.abspath(os.path.dirname(__file__))) # 项目根路径：获取当前路径，再上级路径
+sys.path.append(root_path)  # 将项目根路径写入系统路径
+from utils.general import check_img_size,non_max_suppression_face,scale_coords,xyxy2xywh
+from utils.datasets import letterbox
+from torch2tensorrt.yolo_trt_model import YoloTrtModel
+from detect_face import scale_coords_landmarks,show_results
+cur_path=os.path.abspath(os.path.dirname(__file__))
+def img_process(img_path,long_side=640,stride_max=32):
+    '''
+    图像预处理
+    '''
+    orgimg=cv2.imread(img_path)
+    img0 = copy.deepcopy(orgimg)
+    h0, w0 = orgimg.shape[:2]  # orig hw
+    r = long_side/ max(h0, w0)  # resize image to img_size
+    if r != 1:  # always resize down, only resize up if training with augmentation
+        interp = cv2.INTER_AREA if r < 1 else cv2.INTER_LINEAR
+        img0 = cv2.resize(img0, (int(w0 * r), int(h0 * r)), interpolation=interp)
+
+    imgsz = check_img_size(long_side, s=stride_max)  # check img_size
+
+    img = letterbox(img0, new_shape=imgsz,auto=False)[0] # auto True最小矩形   False固定尺度
+    # Convert
+    img = img[:, :, ::-1].transpose(2, 0, 1).copy()  # BGR to RGB, to 3x416x416
+    img = torch.from_numpy(img)
+    img = img.float()  # uint8 to fp16/32
+    img /= 255.0  # 0 - 255 to 0.0 - 1.0
+    if img.ndimension() == 3:
+        img = img.unsqueeze(0)
+    return img,orgimg
+
+def img_vis(img,orgimg,pred,device,vis_thres = 0.6):
+    '''
+    预测可视化
+    vis_thres: 可视化阈值
+    '''
+
+    print('img.shape: ', img.shape)
+    print('orgimg.shape: ', orgimg.shape)
+
+    no_vis_nums=0
+    # Process detections
+    for i, det in enumerate(pred):  # detections per image
+        gn = torch.tensor(orgimg.shape)[[1, 0, 1, 0]].to(device)  # normalization gain whwh
+        gn_lks = torch.tensor(orgimg.shape)[[1, 0, 1, 0, 1, 0, 1, 0, 1, 0]].to(device)  # normalization gain landmarks
+        if len(det):
+            # Rescale boxes from img_size to im0 size
+            det[:, :4] = scale_coords(img.shape[2:], det[:, :4], orgimg.shape).round()
+
+            # Print results
+            for c in det[:, -1].unique():
+                n = (det[:, -1] == c).sum()  # detections per class
+
+            det[:, 5:15] = scale_coords_landmarks(img.shape[2:], det[:, 5:15], orgimg.shape).round()
+
+            for j in range(det.size()[0]):
+                
+                
+                if det[j, 4].cpu().numpy() < vis_thres:
+                    no_vis_nums+=1
+                    continue
+
+                xywh = (xyxy2xywh(det[j, :4].view(1, 4)) / gn).view(-1).tolist()
+                conf = det[j, 4].cpu().numpy()
+                landmarks = (det[j, 5:15].view(1, 10) / gn_lks).view(-1).tolist()
+                class_num = det[j, 15].cpu().numpy()
+                orgimg = show_results(orgimg, xywh, conf, landmarks, class_num)
+
+    cv2.imwrite(cur_path+'/result.jpg', orgimg)
+    print('result save in '+cur_path+'/result.jpg')
+
+
+if __name__ == '__main__':
+    # ============参数================
+    img_path=cur_path+"/sample.jpg" #测试图片路径
+    device="cuda:0" 
+    onnx_model_path=cur_path+"/yolov5n-0.5.onnx" #ONNX模型路径
+    fp16_mode=True  #True则FP16推理
+
+    # ============图像预处理================
+    img,orgimg=img_process(img_path) #[1,3,640,640]
+    
+    # ============TensorRT推理================
+    # 初始化TensorRT引擎
+    yolo_trt_model=YoloTrtModel(device,onnx_model_path,fp16_mode)
+
+    pred=yolo_trt_model(img.cpu().numpy()) #tensorrt网络推理
+    pred=yolo_trt_model.after_process(pred,device) # torch后处理
+    # Apply NMS
+    pred = non_max_suppression_face(pred, conf_thres=0.3, iou_thres=0.5)
+   
+    # ============可视化================
+    img_vis(img,orgimg,pred,device)
+
+
---
+++ torch2tensorrt/yolo_trt_model.py
@@ -0,0 +1,127 @@
+import os
+import sys
+import onnx
+import pycuda.autoinit
+import pycuda.driver as cuda
+import tensorrt as trt
+import torch
+import time
+import torchvision
+import numpy as np
+import os
+import sys
+from torch2tensorrt.tensorrt_com import Do_Inference,Init_TensorRT,ONNX_to_TensorRT
+
+class YoloTrtModel():
+    '''
+    ONNX->TensorRT并推理
+    '''
+    def __init__(self,device_id="cuda:0",onnx_model_path=None,fp16_mode=False,output_size=(1,-1)):
+        '''
+        device_id: "cuda:0"
+        onnx_model_path: 加载onnx模型的路径
+        output_size: # 输出尺寸 eg:(1,-1) 
+        fp16_mode: True则FP16推理
+        '''
+        trt_engine_path = onnx_model_path.replace('.onnx','.trt')
+
+        # ONNX->TensorRT
+        # ONNX_to_TensorRT(fp16_mode=fp16_mode,onnx_model_path=onnx_model_path,trt_engine_path=trt_engine_path)
+        # 初始化TensorRT
+        self.model_params=Init_TensorRT(trt_engine_path,output_size)
+
+    def __call__(self,img_np_nchw):
+        '''
+        TensorRT推理
+        img_np_nchw: 输入图像 [1,3,640,640]
+        '''
+        context,inputs, outputs, bindings, stream, shape_of_output = self.model_params
+        
+        # 加载输入数据到buffer
+        inputs[0].host = img_np_nchw.reshape(-1) #输入形状转为一维，作为输入
+        # inputs[1].host = ... for multiple input  对于多输入情况
+
+        trt_outputs = Do_Inference(context, bindings=bindings, inputs=inputs, outputs=outputs, stream=stream)  # numpy data
+        stride_8 = trt_outputs[0].reshape(*shape_of_output) # 输出形状由一维转为指定形状
+        stride_16 = trt_outputs[1].reshape(*shape_of_output) # 输出形状由一维转为指定形状
+        stride_32 = trt_outputs[2].reshape(*shape_of_output) # 输出形状由一维转为指定形状
+        return [stride_8,stride_16,stride_32]
+    def after_process(self,pred,device):
+        '''
+        Pytorch后处理
+        pred: tensorrt输出
+        device: "cuda:0"
+        '''
+
+        # 输入尺寸640  降8、16、32倍，对应输出尺寸为80、40、20
+        stride= torch.tensor([8.,16.,32.]).to(device)
+        stride8_shape=(1,3,80,80,16)
+        stride16_shape=(1,3,40,40,16)
+        stride32_shape=(1,3,20,20,16)
+
+        x=[torch.from_numpy(pred[0]).to(device).view(stride8_shape),torch.from_numpy(pred[1]).to(device).view(stride16_shape),torch.from_numpy(pred[2]).to(device).view(stride32_shape)]
+        # 提取自models/yolo.py
+        # ============yolov5-0.5参数 start============
+        nc=1
+        no=16
+
+        nl=3
+        na=3
+        grid=[torch.zeros(1).to(device)] * nl 
+    
+        anchors=torch.tensor([[[ 0.50000,  0.62500],
+            [ 1.00000,  1.25000],
+            [ 1.62500,  2.00000]],
+
+            [[ 1.43750,  1.81250],
+            [ 2.68750,  3.43750],
+            [ 4.56250,  6.56250]],
+
+            [[ 4.56250,  6.78125],
+            [ 7.21875,  9.37500],
+            [10.46875, 13.53125]]]).to(device)
+        
+        anchor_grid=torch.tensor([[[[[[  4.,   5.]]],
+            [[[  8.,  10.]]],
+            [[[ 13.,  16.]]]]],
+            [[[[[ 23.,  29.]]],
+            [[[ 43.,  55.]]],
+            [[[ 73., 105.]]]]],
+            [[[[[146., 217.]]],
+            [[[231., 300.]]],
+            [[[335., 433.]]]]]]).to(device)
+        # ============yolov5-0.5参数 end============
+        
+        z = [] 
+        for i in range(len(x)):
+        
+            bs,ny, nx = x[i].shape[0],x[i].shape[2] ,x[i].shape[3] 
+            if grid[i].shape[2:4] != x[i].shape[2:4]:
+                grid[i] = self._make_grid(nx, ny).to(x[i].device)
+            y = torch.full_like(x[i], 0)
+            y[..., [0,1,2,3,4,15]] = x[i][..., [0,1,2,3,4,15]].sigmoid()
+            y[..., 5:15] = x[i][..., 5:15]
+            #y = x[i].sigmoid()
+
+            y[..., 0:2] = (y[..., 0:2] * 2. - 0.5 + grid[i].to(x[i].device)) * stride[i]  # xy
+            y[..., 2:4] = (y[..., 2:4] * 2) ** 2 * anchor_grid[i]  # wh
+
+            #y[..., 5:15] = y[..., 5:15] * 8 - 4
+            y[..., 5:7]   = y[..., 5:7] *   anchor_grid[i] + grid[i].to(x[i].device) * stride[i] # landmark x1 y1
+            y[..., 7:9]   = y[..., 7:9] *   anchor_grid[i] + grid[i].to(x[i].device) * stride[i]# landmark x2 y2
+            y[..., 9:11]  = y[..., 9:11] *  anchor_grid[i] + grid[i].to(x[i].device) * stride[i]# landmark x3 y3
+            y[..., 11:13] = y[..., 11:13] * anchor_grid[i] + grid[i].to(x[i].device) * stride[i]# landmark x4 y4
+            y[..., 13:15] = y[..., 13:15] * anchor_grid[i] + grid[i].to(x[i].device) * stride[i]# landmark x5 y5
+
+            #y[..., 5:7] = (y[..., 5:7] * 2 -1) * anchor_grid[i]  # landmark x1 y1
+            #y[..., 7:9] = (y[..., 7:9] * 2 -1) * anchor_grid[i]  # landmark x2 y2
+            #y[..., 9:11] = (y[..., 9:11] * 2 -1) * anchor_grid[i]  # landmark x3 y3
+            #y[..., 11:13] = (y[..., 11:13] * 2 -1) * anchor_grid[i]  # landmark x4 y4
+            #y[..., 13:15] = (y[..., 13:15] * 2 -1) * anchor_grid[i]  # landmark x5 y5
+
+            z.append(y.view(bs, -1, no))
+        return torch.cat(z, 1)
+
+    def _make_grid(self,nx=20, ny=20):
+        yv, xv = torch.meshgrid([torch.arange(ny), torch.arange(nx)])
+        return torch.stack((xv, yv), 2).view((1, 1, ny, nx, 2)).float()
